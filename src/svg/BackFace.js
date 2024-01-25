dfcedbbb6fe0c63b019db35c21b83c6d99106067
jolka <firjola@gmail.com>
2024-01-25 19:25:42 +0100

    improved styles

--- src/svg/BackFace.js
+++ src/svg/BackFace.js
@@ -1,6 +1,11 @@
 export const BackFace = ({ className }) => {
   return (
-    <svg className={className} viewBox="0 0 900 600">
+    <svg
+      className={className}
+      viewBox="0 0 900 600"
+      stroke="#103363"
+      stroke-width="10%"
+    >
       <rect width="900" height="600" fill="#093d72"></rect>
     </svg>
   );
